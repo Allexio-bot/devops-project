f80ea22b9bee023ebcfc6c674c2900267c23d377
Tristan Dietz <tristan.dietz@laposte.net>
2020-06-05 02:02:18 +0200

    Debugging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 
 cd /bot/src
+
+echo "Avant le if"
 if [ "${discord_sot_token}" = "" ]; then
     cat /bot/discord_token
     export discord_sot_token=$(cat /bot/discord_token)
 fi
+echo "Après le if"
 
 python3 bot.py
