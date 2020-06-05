0ad8658e16bb9e7f0931b46635b6cbc07d7493d3
Tristan Dietz <tristan.dietz@laposte.net>
2020-06-05 02:29:28 +0200

    Removing traces for trying to use Kube secret

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,14 @@
 
 cd /bot/src
 
-echo "Avant le if"
-if [ "${discord_sot_token}" = "" ]; then
-    cat /bot/discord_token
-    export discord_sot_token=$(cat /bot/discord_token)
-fi
-echo "Après le if: ${discord_sot_token}"
-ls /bot
+#echo "Avant le if"
+#if [ "${discord_sot_token}" = "" ]; then
+#    cat /bot/discord_token
+#    export discord_sot_token=$(cat /bot/discord_token)
+#fi
+#echo "Après le if: ${discord_sot_token}"
+#ls /bot
+
+export discord_sot_token=$DISCORD_SOT_TOKEN
 
 python3 bot.py
