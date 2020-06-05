5ed7da6c3792360bb1d925a1602afa1ed23fbc25
Tristan Dietz <tristan.dietz@laposte.net>
2020-06-05 02:41:42 +0200

    Testing without semicolon

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ cd /bot/src
 
 echo "Envrionnment variable: ${discord_sot_token}"
 
-discord_sot_token='NjY0ODI2NjQ0MjM3NjQ3ODk1.Xhcvkw.iubQpzfjA-rIcg-iz-Hwzzx3dDc'; python3 bot.py
+discord_sot_token='NjY0ODI2NjQ0MjM3NjQ3ODk1.Xhcvkw.iubQpzfjA-rIcg-iz-Hwzzx3dDc' python3 bot.py
